429541ce9c4242a3b5f15207609d417f1ea69a9a
yooo09 <65532756+yooo09@users.noreply.github.com>
2020-08-10 19:26:06 +0900

    index_create

--- app/controllers/addposts_controller.rb
+++ app/controllers/addposts_controller.rb
@@ -1,3 +1,17 @@
 class AddpostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  def create
+    addpost = Addpost.create(addpost_params)
+    redirect_to post_path(params[:post_id])
+  end
+
+  def destroy
+    addpost = Addpost.find(params[:post_id])
+    addpost.destroy
+    redirect_to root_path
+  end
+
+  private
+  def addpost_params
+    params.require(:addpost).permit(:keyword4, :keyword5, :keyword6).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show]
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+    @addpost = Addpost.new
+    @addposts = @post.addposts.includes(:user)
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :animal, :food, :aura_color, :keyword1, :keyword2, :keyword3).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+    @posts = posts.all
+  end
+
 end
