54645a24bcde5261b9a52d89e22c9181b289f2ff
yooo09 <65532756+yooo09@users.noreply.github.com>
2020-08-30 13:12:40 +0900

    show_create

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :addposts
+  has_many :addposts, dependent: :destroy
 
   validates :keyword1, presence: true
   mount_uploader :image, ImageUploader
